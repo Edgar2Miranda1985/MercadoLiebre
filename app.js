24ce105302e83f7c30a35dc4681ac3f30c28c565
Edgar2Miranda1985 <edgardo1166@gmail.com>
2023-01-19 02:49:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express(); 
+const PORT = process.env.PORT || 3000;  
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.listen(PORT, ()=>{
+    console.log('servidor funcionando' + PORT);
+});
